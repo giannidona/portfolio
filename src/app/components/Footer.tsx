1d072e2f35cdba154d621ba177719ec304b805c3
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-01-24 17:02:33 -0300

    footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,31 @@
+export default function Footer() {
+  return (
+    <footer className="w-3/5 mx-auto mt-16">
+      <h3 className="text-md font-bold mb-3">CONTACTAME</h3>
+      <div className="mb-2">
+        <span className="mr-12 text-stone-500">Email</span>
+        <a className="ml-5" href="mailto:gianlucadonato2005@gmail.com">
+          gianlucadonato2005@gmail.com
+        </a>
+      </div>
+      <div className="mb-2">
+        <span className="mr-10 text-stone-500">Github</span>
+        <a className="ml-5" href="https://github.com/giannidona">
+          giannidona
+        </a>
+      </div>
+      <div className="mb-2">
+        <span className="mr-9 text-stone-500">Twitter</span>
+        <a className="ml-5" href="https://twitter.com/giannidona_dev">
+          @giannidona_dev
+        </a>
+      </div>
+      <div className="mb-2">
+        <span className="mr-7 text-stone-500">Linkedin</span>
+        <a className="ml-5" href="https://www.linkedin.com/in/gianidonato/">
+          in/gianidonato
+        </a>
+      </div>
+    </footer>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Main from "./components/Main";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Main />
       <Projects />
+      <Footer />
     </>
   );
 }

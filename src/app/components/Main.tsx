142a2d7c97080f6d11aa5a076d2e12fac75c7e6f
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-01-24 15:54:30 -0300

    section main

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -2,9 +2,19 @@ export default function Main() {
   return (
     <>
       <section className="text-center my-40">
-        <p>Hola, todo bien?👋</p>
-        <h2>Gianluca Donato</h2>
-        <p>Desarrollador web</p>
+        <h1 className="text-5xl font-bold mb-5">Gianluca Donato</h1>
+        <p className="text-xl">Desarrollador web trainee.</p>
+        <p className="text-xl">
+          Para saber mas mi de tengo un{" "}
+          <a
+            className="font-bold underline"
+            href="https://giannidona-newsletter.netlify.app/"
+            target="_blank"
+          >
+            newsletter
+          </a>
+          .
+        </p>
       </section>
     </>
   );

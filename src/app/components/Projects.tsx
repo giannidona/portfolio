d89d45322056486aa0f21df2913c64ce4e83aeb8
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-01-24 16:45:46 -0300

    projects section

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,5 +1,8 @@
 import projects from "../../../data/projects.json";
 
+import { LuExternalLink } from "react-icons/lu";
+import { FiGithub } from "react-icons/fi";
+
 export default function Projects() {
   return (
     <section>
@@ -7,30 +10,36 @@ export default function Projects() {
       <div>
         {projects.map((project) => (
           <div
-            className="border-2 border-white w-3/5 m-auto mb-5 p-2"
+            className="border-2 rounded-md border-white w-3/5 m-auto mb-5 p-2"
             key={project.id}
           >
-            <p className="font-bold mb-1">{project.title}</p>
-            <p className="text-stone-500 italic mb-1">{project.description}</p>
-            <p className="mb-1">{project.technoliges}</p>
-            {project.github && (
-              <a
-                className="mb-1 hover:text-neutral-300 pr-5"
-                href={project.github}
-                target="_blank"
-              >
-                code
-              </a>
-            )}
-            {project.link && (
-              <a
-                className="mb-1 hover:text-neutral-300"
-                href={project.link}
-                target="_blank"
-              >
-                demo
-              </a>
-            )}
+            <div className="flex justify-between">
+              <div className="flex my-auto">
+                <p className="font-bold">{project.title}</p>
+                {project.demo && (
+                  <a
+                    className="text-md mx-2 my-auto"
+                    href={project.demo}
+                    target="_blank"
+                  >
+                    <LuExternalLink />
+                  </a>
+                )}
+                {project.code && (
+                  <a
+                    className="text-md mx-2 my-auto"
+                    href={project.code}
+                    target="_blank"
+                  >
+                    <FiGithub />
+                  </a>
+                )}
+              </div>
+            </div>
+            <p className="text-stone-500 italic my-2">{project.description}</p>
+            <p className="mb-1">
+              {project.technoliges.toLocaleString().replaceAll(",", " ")}
+            </p>
           </div>
         ))}
       </div>

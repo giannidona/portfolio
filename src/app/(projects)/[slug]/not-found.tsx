96c4f3852ed453ae2d80afde93dd8a2e0fe32dd7
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-06-03 20:33:07 -0300

    project details

---
+++ src/app/(projects)/[slug]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function ProjectNotFoundPage() {
+  return (
+    <div className="h-screen text-center mt-20">
+      <h1 className="font-semibold">
+        Sorry, but at this moment, this project does not exist.
+      </h1>
+    </div>
+  );
+}

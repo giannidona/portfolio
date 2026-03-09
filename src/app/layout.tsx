41c94c5063e47ae8f8759dfa4fa2297760fa3c41
giannidona <gianlucadonato2005@gmail.com>
2026-03-08 22:42:54 -0300

    feat: add Open Graph and Twitter metadata for improved SEO and social sharing, and include banner image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,29 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Gianluca Donato",
   description: "Web Developer & Designer based in Buenos Aires, Argentina.",
+  openGraph: {
+    title: "Gianluca Donato",
+    description: "Web Developer & Designer based in Buenos Aires, Argentina.",
+    url: "https://gianluca-donato.vercel.app/en", // Puedes actualizar esto con tu dominio real si es distinto
+    siteName: "Gianluca Donato",
+    images: [
+      {
+        url: "/bannner-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Gianluca Donato - Web Developer & Designer",
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Gianluca Donato",
+    description: "Web Developer & Designer based in Buenos Aires, Argentina.",
+    images: ["/bannner-meta.jpg"],
+    creator: "@giannidona_dev",
+  },
 };
 
 export default function RootLayout({
